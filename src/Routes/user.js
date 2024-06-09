4ab96f834c89c3e5daad18471ab33d50e96dccbc
Vipin Patel <vp923vin@gmail.com>
2024-06-09 08:52:15 +0530

    authentication is done and middleware is set

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { sendWelcomeEmail } = require('../Controllers/testController');
 const { getProfile, updateProfile } = require('../Controllers/UserController');
+const authMiddleware = require('../Middlewares/authMiddleware');
+const roleMiddleware = require('../Middlewares/roleMiddleware');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -8,7 +11,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/welcome-email', sendWelcomeEmail);
-router.get('/profile/:user_id', getProfile);
-router.put('/update-profile/:user_id', updateProfile);
+router.get('/profile/:user_id', authMiddleware, roleMiddleware('customer'), getProfile);
+router.put('/update-profile/:user_id', authMiddleware, roleMiddleware('customer'), updateProfile);
 
 module.exports = router;
