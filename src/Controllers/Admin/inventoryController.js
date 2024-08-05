c9eff2fcbbe84911fad8c1a43129eeffeba5c849
Vipin Patel <vp923vin@gmail.com>
2024-08-06 01:26:28 +0530

    inventory functionality done

--- src/Controllers/Admin/inventoryController.js
+++ src/Controllers/Admin/inventoryController.js
@@ -1,30 +1,29 @@
-const { validationResult } = require('express-validator');
-const Inventory = require('../Models/Inventory');
-const Product = require('../Models/Product');
-const formatErrors = require('../Services/Utils/formErrorFormat');
+const { Inventory, Product } = require('../../Models/Index');
 
-const createInventory = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const formattedErrors = formatErrors(errors.array());
-        return res.status(400).json({
-            status: 'failed',
-            statusCode: 400,
-            message: "Validation Failed",
-            errors: formattedErrors,
-        });
-    }
-
-    const { product_id, quantity, min_quantity, max_quantity, warehouse_location } = req.body;
-
+const createOrUpdateInventory = async (req, res) => {
+    const { product_id, available_quantity, stock_status } = req.body;
     try {
-        const inventory = await Inventory.create({ product_id, quantity, min_quantity, max_quantity, warehouse_location });
-        return res.status(201).json({
-            status: 'success',
-            statusCode: 201,
-            message: 'Inventory record created successfully',
-            data: { inventory: inventory },
-        });
+        const existingInventory = await Inventory.findOne({ where: { product_id } });
+        
+        if (existingInventory) {
+            existingInventory.available_quantity = available_quantity;
+            existingInventory.stock_status = stock_status;
+            await existingInventory.save();
+            return res.status(200).json({
+                status: 'success',
+                statusCode: 200,
+                message: 'Inventory record updated successfully',
+                data: { inventory: existingInventory },
+            });
+        } else {
+            const inventory = await Inventory.create({ product_id, available_quantity, stock_status });
+            return res.status(201).json({
+                status: 'success',
+                statusCode: 201,
+                message: 'Inventory record created successfully',
+                data: { inventory: inventory },
+            });
+        }
     } catch (error) {
         return res.status(500).json({
             status: 'failed',
@@ -38,10 +37,7 @@ const createInventory = async (req, res) => {
 const getInventorieById = async (req, res) => {
     const { inventoryId } = req.params;
     try {
-        const inventory = await Inventory.findOne({
-            where: { inventory_id: inventoryId },
-            include: [{ model: Product, as: 'product' }],
-        });
+        const inventory = await Inventory.findOne({ where: { inventory_id: inventoryId } });
         if (!inventory) {
             return res.status(404).json({
                 status: 'failed',
@@ -67,18 +63,8 @@ const getInventorieById = async (req, res) => {
 };
 
 const updateInventory = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const formattedErrors = formatErrors(errors.array());
-        return res.status(400).json({
-            status: 'failed',
-            statusCode: 400,
-            message: "Validation Failed",
-            errors: formattedErrors,
-        });
-    }
     const { inventoryId } = req.params;
-    const { quantity, min_quantity, max_quantity, warehouse_location } = req.body;
+    const { product_id, available_quantity, stock_status } = req.body;
     try {
         const inventory = await Inventory.findByPk(inventoryId);
         if (!inventory) {
@@ -89,10 +75,9 @@ const updateInventory = async (req, res) => {
                 errors: [{ message: 'Inventory record not found' }]
             });
         }
-        inventory.quantity = quantity;
-        inventory.min_quantity = min_quantity;
-        inventory.max_quantity = max_quantity;
-        inventory.warehouse_location = warehouse_location;
+        inventory.product_id = !(product_id) ? product_id : null;
+        inventory.available_quantity = available_quantity;
+        inventory.stock_status = stock_status;
         await inventory.save();
         return res.status(200).json({
             status: 'success',
@@ -110,7 +95,7 @@ const updateInventory = async (req, res) => {
     }
 };
 
-const deleteInventory = async (req, res) => {
+const tempDeleteInventory = async (req, res) => {
     const { inventoryId } = req.params;
     try {
         const inventory = await Inventory.findByPk(inventoryId);
@@ -121,11 +106,12 @@ const deleteInventory = async (req, res) => {
                 message: 'Inventory record not found',
             });
         }
-        await inventory.destroy();
+        inventory.deletedAt = Date.now();
+        await inventory.save();
         return res.status(200).json({
             status: 'success',
             statusCode: 200,
-            message: 'Inventory record deleted successfully',
+            message: 'Inventory record moved to trash successfully',
         });
     } catch (error) {
         return res.status(500).json({
@@ -137,7 +123,7 @@ const deleteInventory = async (req, res) => {
     }
 };
 
-const updateLastRestocked = async (req, res) => {
+const updateStockStatus = async (req, res) => {
     const { inventoryId } = req.params;
     try {
         const inventory = await Inventory.findByPk(inventoryId);
@@ -150,7 +136,12 @@ const updateLastRestocked = async (req, res) => {
             });
         }
 
-        inventory.last_restocked = new Date();
+        if(inventory.available_quantity <= 0) {
+            inventory.stock_status = 'Out';
+        }else{
+            inventory.stock_status = 'In';
+        }
+        inventory.updatedAt = new Date();
         await inventory.save();
 
         return res.status(200).json({
@@ -169,44 +160,165 @@ const updateLastRestocked = async (req, res) => {
     }
 };
 
-const updateLastSold = async (req, res) => {
-    const { inventoryId } = req.params;
+const getInventoryByPagination = async (req, res) => {
+    const { page = 1, pageSize = 10 } = req.query;
+    const offset = (page - 1) * pageSize;
+    const limit = parseInt(pageSize);
 
     try {
-        const inventory = await Inventory.findByPk(inventoryId);
-        if (!inventory) {
-            return res.status(404).json({
-                status: 'failed',
-                statusCode: 404,
-                message: 'Inventory not found',
-                errors: [{ message: 'Inventory not found' }]
-            });
-        }
-
-        inventory.last_sold =new Date();
-        await inventory.save();
+        const inventories = await Inventory.findAndCountAll({
+            offset: offset,
+            limit: limit,
+        });
 
         return res.status(200).json({
             status: 'success',
             statusCode: 200,
-            message: 'Last sold date updated successfully',
-            data: { inventory: inventory },
+            data: {
+                totalItems: inventories.count,
+                totalPages: Math.ceil(inventories.count / limit),
+                currentPage: parseInt(page),
+                inventories: inventories.rows,
+            },
         });
     } catch (error) {
         return res.status(500).json({
             status: 'failed',
             statusCode: 500,
-            message: 'Something went wrong in server.',
-            error: error.message
+            message: 'Something went wrong on the server.',
+            error: error.message,
+        });
+    }
+};
+
+const tempBulkDeleteInventory = async (req, res) => {
+    const { ids } = req.body; // Array of inventory IDs to be soft deleted
+
+    try {
+        await Inventory.update(
+            { deletedAt: new Date() }, // Soft delete
+            { where: { id: ids } }
+        );
+
+        return res.status(200).json({
+            status: 'success',
+            statusCode: 200,
+            message: 'Inventory records temporarily deleted successfully.',
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'failed',
+            statusCode: 500,
+            message: 'Something went wrong on the server.',
+            error: error.message,
+        });
+    }
+};
+
+const permanentDeleteInventoryById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Inventory.destroy({
+            where: { id: id },
+        });
+
+        return res.status(200).json({
+            status: 'success',
+            statusCode: 200,
+            message: 'Inventory record permanently deleted successfully.',
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'failed',
+            statusCode: 500,
+            message: 'Something went wrong on the server.',
+            error: error.message,
+        });
+    }
+};
+
+const permanentMultiDeleteInventory = async (req, res) => {
+    const { ids } = req.body; // Array of inventory IDs to be permanently deleted
+
+    try {
+        await Inventory.destroy({
+            where: { id: ids },
+        });
+
+        return res.status(200).json({
+            status: 'success',
+            statusCode: 200,
+            message: 'Inventory records permanently deleted successfully.',
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'failed',
+            statusCode: 500,
+            message: 'Something went wrong on the server.',
+            error: error.message,
+        });
+    }
+};
+
+const restoreInventory = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Inventory.update(
+            { deletedAt: null }, // Restore
+            { where: { id: id } }
+        );
+
+        return res.status(200).json({
+            status: 'success',
+            statusCode: 200,
+            message: 'Inventory record restored successfully.',
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'failed',
+            statusCode: 500,
+            message: 'Something went wrong on the server.',
+            error: error.message,
+        });
+    }
+};
+
+const restoreMultipleInventory = async (req, res) => {
+    const { ids } = req.body; // Array of inventory IDs to be restored
+
+    try {
+        await Inventory.update(
+            { deletedAt: null }, // Restore
+            { where: { id: ids } }
+        );
+
+        return res.status(200).json({
+            status: 'success',
+            statusCode: 200,
+            message: 'Inventory records restored successfully.',
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'failed',
+            statusCode: 500,
+            message: 'Something went wrong on the server.',
+            error: error.message,
         });
     }
 };
 
 module.exports = {
-    createInventory,
+    createOrUpdateInventory,
     getInventorieById,
     updateInventory,
-    deleteInventory,
-    updateLastRestocked,
-    updateLastSold
+    tempDeleteInventory,
+    updateStockStatus,
+    getInventoryByPagination,
+    tempBulkDeleteInventory,
+    permanentDeleteInventoryById,
+    permanentMultiDeleteInventory,
+    restoreInventory,
+    restoreMultipleInventory
 };
--- src/Models/Inventory.js
+++ src/Models/Inventory.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     product_id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: true,
     },
     available_quantity: {
       type: DataTypes.INTEGER,
